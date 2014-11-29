830c0d2a65f9a38eed2a5ccae293d91c25cbbafa
Luis Borjas <luisfborjas@gmail.com>
2014-11-29 01:12:34 -0600

    agrega filtros al ejercicio de tablas

--- codigo/tablas.js
+++ codigo/tablas.js
@@ -23,15 +23,42 @@ TableCreator.init = function(form, table){
 TableCreator.populateTable = function(event){
   event.preventDefault();
   var data = JSON.parse(TableCreator.form.find("#data").val()); 
+  var headers = TableCreator.helpers.extractHeaders(data);
   //eliminar cualquier tabla anterior
   TableCreator.table.empty();
   //agregar los encabezados:
   TableCreator.table.append(
-    ich.headers({headers: TableCreator.helpers.extractHeaders(data)})
+    ich.headers({headers: headers})
   );
   //agregar los datos
   TableCreator.table.append(
     ich.body({data: TableCreator.helpers.extractRows(data)})
   );
+  //preparar los filtros
+  TableCreator.prepareFilters(data);
+}
+
+TableCreator.prepareFilters = function(data){
+  var headers = TableCreator.helpers.extractHeaders(data);
+
+  $("#data-filter").html(ich.filter({options: headers}));
+
+  $("#data-reset").click(function(event){
+    event.preventDefault();
+    $("tr").show();
+  })
+
+  $("#data-search").submit(function(event){
+    event.preventDefault();
+    var query = $("#data-query").val();
+    var field = $("#data-filter").val();
+    var columnNumber = headers.indexOf(field)+1;
+    console.log(query, field, columnNumber)
+    TableCreator.table.find(
+      "td:nth-child("+columnNumber+")"
+    ).not(
+      ":contains("+query+")"
+    ).parent().hide();
+  })
 }
 
